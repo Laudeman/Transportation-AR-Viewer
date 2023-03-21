0c8c2328427f947a5c649ca0c5fc84fa902985b3
Bobby <bobbylaudeman@gmail.com>
2023-03-21 15:29:00 -0700

    site only

--- transportation-ar-viewer/src/App.js
+++ transportation-ar-viewer/src/App.js
@@ -1,24 +1,47 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "@google/model-viewer";
 
 function App() {
 	return (
-		<div className="App">
-			<model-viewer
-				id="mv-demo"
-				shadow-intensity="1"
-				src="truck.glb"
-				alt="A 3D model of an astronaut"
-				style={{
-					width: "100%",
-					height: "100vh",
-				}}
-				auto-rotate
-				ar
-				camera-controls
-				poster="./truck.jpg"
-			></model-viewer>
+		<div
+			className="App"
+			style={{ display: "block", "text-align": "center" }}
+		>
+			<h1 height="5vh">Transportation</h1>
+			<div>
+				<h2>Old Truck</h2>
+				<model-viewer
+					id="mv-demo"
+					shadow-intensity="1"
+					src="truck.glb"
+					alt="An old truck"
+					style={{
+						display: "block",
+						left: "33%",
+					}}
+					ar
+					auto-rotate
+					camera-controls
+					poster="./truck.jpg"
+				></model-viewer>
+			</div>
+			<div>
+				<h2>Steam Boat</h2>
+				<model-viewer
+					id="mv-demo"
+					shadow-intensity="1"
+					src="truck.glb"
+					alt="An old truck"
+					style={{
+						display: "block",
+						left: "33%",
+					}}
+					ar
+					auto-rotate
+					camera-controls
+					poster="./truck.jpg"
+				></model-viewer>
+			</div>
 		</div>
 	);
 }
